566b9ed5b4028eb219c82fb4dbc769c0a2f820e8
iammarshallnz <marshallinch@gmail.com>
2026-03-03 12:37:40 +1300

    Refactored + added functionality to js

---
+++ src/blackhole.rs
@@ -0,0 +1,50 @@
+use nalgebra::Vector2;
+use crate::common;
+
+/// Blackhole struct
+/// Can only have 1 blackhole due to using non Newtonian method for the step
+
+pub struct Blackhole {
+    pub pos: Vector2<f64>,
+    pub mass: f64,
+    pub r_s: f64, // event horizon
+}
+impl Blackhole {
+    pub fn new(pos: Vector2<f64>, mass: f64) -> Blackhole {
+        Blackhole {
+            pos,
+            mass,
+            r_s: (2.0 * common::G * mass) / (common::C * common::C),
+        }
+    }
+    pub fn draw(
+        &self,
+        buffer: &mut [u8],
+        width: usize,
+        height: usize,
+        scale: f64,
+        offset: Vector2<f64>,
+    ) {
+        let cx = ((self.pos.x - offset.x) / scale) as i32 + (width / 2) as i32;
+        let cy = ((self.pos.y - offset.y) / scale) as i32 + (height / 2) as i32;
+        let radius = (self.r_s / scale) as i32;
+
+        for y in -radius..=radius {
+            for x in -radius..=radius {
+                if x * x + y * y <= radius * radius {
+                    let px = cx + x;
+                    let py = cy + y;
+
+                    if px >= 0 && px < width as i32 && py >= 0 && py < height as i32 {
+                        let idx = (py as usize * width + px as usize) * 4;
+
+                        buffer[idx] = 255;
+                        buffer[idx + 1] = 0;
+                        buffer[idx + 2] = 0;
+                        buffer[idx + 3] = 255;
+                    }
+                }
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,20 @@
 #![allow(non_snake_case)]
+mod blackhole;
 mod common;
+mod ray;
+mod step;
 
-use nalgebra::{Rotation2, Vector2, Vector4};
-use std::collections::VecDeque;
+use nalgebra::Vector2;
+
 use wasm_bindgen::prelude::*;
+
+use crate::{blackhole::Blackhole, ray::Ray};
 
 const WIDTH: usize = 300;
 const HEIGHT: usize = 300;
+
+/// Rendering for the blackhole and rays.
+/// plus comunicating with js front end 
 
 // macro_rules! console_log {
 //     // Note that this is using the `log` function imported above during
@@ -19,198 +27,6 @@ extern "C" {
     pub fn alert(s: &str);
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-
-struct Blackhole {
-    pos: Vector2<f64>,
-    r_s: f64, // event horizon
-}
-impl Blackhole {
-    fn new(pos: Vector2<f64>, mass: f64) -> Blackhole {
-        Blackhole {
-            pos,
-            r_s: (2.0 * common::G * mass) / (common::C * common::C),
-        }
-    }
-    fn draw(
-        &self,
-        buffer: &mut [u8],
-        width: usize,
-        height: usize,
-        scale: f64,
-        offset: Vector2<f64>,
-    ) {
-        let cx = ((self.pos.x - offset.x) / scale) as i32 + (width / 2) as i32;
-        let cy = ((self.pos.y - offset.y) / scale) as i32 + (height / 2) as i32;
-        let radius = (self.r_s / scale) as i32;
-
-        for y in -radius..=radius {
-            for x in -radius..=radius {
-                if x * x + y * y <= radius * radius {
-                    let px = cx + x;
-                    let py = cy + y;
-
-                    if px >= 0 && px < width as i32 && py >= 0 && py < height as i32 {
-                        let idx = (py as usize * width + px as usize) * 4;
-
-                        buffer[idx] = 255;
-                        buffer[idx + 1] = 0;
-                        buffer[idx + 2] = 0;
-                        buffer[idx + 3] = 255;
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct Ray {
-    // -- Cartesian coords -- //
-    pub pos: Vector2<f64>,
-
-    // -- Polar coords -- //
-    pub r: f64,
-    pub phi: f64,
-    pub dr: f64,
-    pub dphi: f64,
-
-    // Trail of points
-    pub trail: VecDeque<Vector2<f64>>,
-
-    // Conserved quantities
-    pub E: f64,
-}
-
-impl Ray {
-    pub fn new(pos: Vector2<f64>, dir: Vector2<f64>, bh_pos: Vector2<f64>, r_s: f64) -> Ray {
-        let rel = pos - bh_pos; // relative to black hole
-        let x = rel.x;
-        let y = rel.y;
-        let r = rel.norm();
-        let phi = y.atan2(x);
-
-        let radial = rel.normalize();
-        let tangential = Vector2::new(-radial.y, radial.x);
-
-        let dr = dir.dot(&radial);
-        let dphi = dir.dot(&tangential) / r;
-
-        let f = 1.0 - r_s / r;
-        let dt_dlambda = ((dr * dr) / (f * f) + (r * r * dphi * dphi) / f).sqrt();
-        let E = f * dt_dlambda;
-
-        let mut trail = VecDeque::new();
-        trail.push_back(pos);
-
-        Ray {
-            pos,
-            r,
-            phi,
-            dr,
-            dphi,
-            trail,
-            E,
-        }
-    }
-    fn draw(
-        &mut self,
-        buffer: &mut [u8],
-        width: usize,
-        height: usize,
-        scale: f64,
-        offset: Vector2<f64>,
-    ) {
-        let x = ((self.pos.x - offset.x) / scale) as i32 + (width / 2) as i32;
-        let y = ((self.pos.y - offset.y) / scale) as i32 + (height / 2) as i32;
-        // push to trail (limit size for performance)
-        const MAX_TRAIL: usize = 200;
-        self.trail.push_back(self.pos);
-        if self.trail.len() > MAX_TRAIL {
-            self.trail.pop_front();
-        }
-        if x >= 0 && x < width as i32 && y >= 0 && y < height as i32 {
-            let idx = (y as usize * width + x as usize) * 4;
-
-            buffer[idx] = 255; // R
-            buffer[idx + 1] = 255; // G
-            buffer[idx + 2] = 255; // B
-            buffer[idx + 3] = 255; // A
-        }
-    }
-    fn step(&mut self, d_lambda: f64, bh: &Blackhole) {
-        if (self.pos - bh.pos).norm() <= bh.r_s {
-            return;
-        } // inside event horizon
-
-        // RK4 integration
-        rk4_step(self, d_lambda, bh.r_s);
-
-        // update Cartesian for drawing
-        let rot = Rotation2::new(self.phi);
-        let cart = rot * Vector2::new(self.r, 0.0);
-
-        self.pos = cart + bh.pos;
-    }
-
-    fn draw_trail(
-        &self,
-        buffer: &mut [u8],
-        width: usize,
-        height: usize,
-        scale: f64,
-        offset: Vector2<f64>,
-    ) {
-        let size = self.trail.len();
-        for (index, point) in self.trail.iter().enumerate() {
-            let x = ((point.x - offset.x) / scale) as i32 + (width / 2) as i32;
-            let y = ((point.y - offset.y) / scale) as i32 + (height / 2) as i32;
-            let ratio: f64 = index as f64 / size as f64;
-            if x >= 0 && x < width as i32 && y >= 0 && y < height as i32 {
-                let idx = (y as usize * width + x as usize) * 4;
-
-                buffer[idx] = (200.0 * ratio) as u8;
-                buffer[idx + 1] = (200.0 * ratio) as u8;
-                buffer[idx + 2] = (200.0 * ratio) as u8;
-                buffer[idx + 3] = 255;
-            }
-        }
-    }
-}
-
-fn geodesic_rhs_from_state(state: &Vector4<f64>, E: f64, r_s: f64) -> Vector4<f64> {
-    let r = state[0];
-    let dr = state[2];
-    let dphi = state[3];
-
-    let f = 1.0 - r_s / r;
-    let dt_dλ = E / f;
-
-    let d2r = -(r_s / (2.0 * r * r)) * f * (dt_dλ * dt_dλ)
-        + (r_s / (2.0 * r * r * f)) * (dr * dr)
-        + (r - r_s) * (dphi * dphi);
-
-    let d2phi = -2.0 * dr * dphi / r;
-
-    Vector4::new(dr, dphi, d2r, d2phi)
-}
-
-fn rk4_step(ray: &mut Ray, d_lambda: f64, r_s: f64) {
-    let y0 = Vector4::new(ray.r, ray.phi, ray.dr, ray.dphi);
-
-    let k1 = geodesic_rhs_from_state(&y0, ray.E, r_s);
-    let k2 = geodesic_rhs_from_state(&(y0 + k1 * (d_lambda / 2.0)), ray.E, r_s);
-    let k3 = geodesic_rhs_from_state(&(y0 + k2 * (d_lambda / 2.0)), ray.E, r_s);
-    let k4 = geodesic_rhs_from_state(&(y0 + k3 * d_lambda), ray.E, r_s);
-
-    ray.r += (d_lambda / 6.0) * (k1[0] + 2.0 * k2[0] + 2.0 * k3[0] + k4[0]);
-    ray.phi += (d_lambda / 6.0) * (k1[1] + 2.0 * k2[1] + 2.0 * k3[1] + k4[1]);
-    ray.dr += (d_lambda / 6.0) * (k1[2] + 2.0 * k2[2] + 2.0 * k3[2] + k4[2]);
-    ray.dphi += (d_lambda / 6.0) * (k1[3] + 2.0 * k2[3] + 2.0 * k3[3] + k4[3]);
 }
 
 #[wasm_bindgen]
@@ -227,10 +43,11 @@ impl Renderer {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Renderer {
         let scale = 1e9; // 1 pixel per
-        let offset = Vector2::new(150.0, 150.0); // center in world coordinates
+        let offset = Vector2::new(0.0, 0.0); // center in world coordinates
 
         let mut rays = Vec::new();
         let blackhole = Blackhole::new(Vector2::new(0.0, 0.0), 8.54e36);
+        #[allow(clippy::excessive_precision)]
         rays.push(Ray::new(
             // cool cycle
             Vector2::new(-1e11, 3.27608302719999999e10),
@@ -241,19 +58,41 @@ impl Renderer {
 
         Renderer {
             buffer: vec![0; WIDTH * HEIGHT * 4],
-            blackhole: blackhole,
+            blackhole,
             rays,
             scale,
             offset,
         }
     }
-
+    // For js to access buffer
     pub fn buffer_ptr(&self) -> *const u8 {
         self.buffer.as_ptr()
     }
 
+    pub fn add_ray_from_click(&mut self, mouse_x: f64, mouse_y: f64) {
+        // Convert screen (pixels) to world coordinates
+        let center = Vector2::new(WIDTH as f64 / 2.0, HEIGHT as f64 / 2.0);
+
+        let screen = Vector2::new(mouse_x, mouse_y);
+
+        let world_pos = (screen - center) * self.scale + self.offset;
+        if (world_pos - self.blackhole.pos).norm() <= self.blackhole.r_s {
+            return;
+        }
+        let dir = Vector2::new(common::C, 0.0);
+
+        let ray = Ray::new(world_pos, dir, self.blackhole.pos, self.blackhole.r_s);
+
+        self.rays.push(ray);
+    }
+    
+    pub fn set_blackhole_mass(&mut self, mass: f64) {
+        self.blackhole.mass = mass;
+        self.blackhole.r_s = (2.0 * common::G * mass) / (common::C * common::C);
+    }
+
     pub fn update(&mut self) {
-        // Clear
+        // Clear screen
         for i in 0..WIDTH * HEIGHT {
             let idx = i * 4;
             self.buffer[idx] = 0;
@@ -262,17 +101,32 @@ impl Renderer {
             self.buffer[idx + 3] = 255;
         }
 
-        for ray in &mut self.rays {
+        self.rays.retain_mut(|ray| {
             let steps_per_frame = 20;
             let dt = 0.1;
             for _ in 0..steps_per_frame {
                 ray.step(dt, &self.blackhole);
             }
+
             ray.draw_trail(&mut self.buffer, WIDTH, HEIGHT, self.scale, self.offset);
+
+            let max_radius = 3.0e11;
+
             ray.draw(&mut self.buffer, WIDTH, HEIGHT, self.scale, self.offset);
-        }
+            if (ray.pos - self.blackhole.pos).norm() > max_radius {
+                false
+            } else {
+                true
+            }
+        });
 
         self.blackhole
             .draw(&mut self.buffer, WIDTH, HEIGHT, self.scale, self.offset);
+    }
+}
+
+impl Default for Renderer {
+    fn default() -> Self {
+        Self::new()
     }
 }
